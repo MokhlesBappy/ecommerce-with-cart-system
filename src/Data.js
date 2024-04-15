73e2f4f6ce3f219fbe96c013853b8f67396489f4
MokhlesBappy <158749484+MokhlesBappy@users.noreply.github.com>
2024-04-15 14:58:08 +0600

    ecommerce with cart system
    ecommerce with cart system

---
+++ src/Data.js
@@ -0,0 +1,44 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Onion",
+    price: 500,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-1.png",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Local Masala",
+    price: 600,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-2.png",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "Radhuni Mix",
+    price: 700,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-3.png",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Radhuni Masala",
+    price: 800,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-4.png",
+  },
+  {
+    id: "thyfhcbcw",
+    name: "Detergen",
+    price: 900,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-5.png",
+  },
+  {
+    id: "aaa",
+    name: "Masala",
+    price: 1100,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-4.png",
+  },
+];
